d113c822d7debc80e2af9013d7715118afd19502
Martin <martin.rog0404@gmail.com>
2018-11-05 00:15:28 +0000

    Adding mocha test

---
+++ test/basic.test.js
@@ -0,0 +1,10 @@
+var supertest = require('supertest');
+var app = require('../app');
+
+describe("Header test", function(){
+    it("Display header based on URL parameter", function(done){
+        supertest(app).get("/start/1338")
+        .expect(200)
+        .expect(/1338/, done)
+    })
+});  
