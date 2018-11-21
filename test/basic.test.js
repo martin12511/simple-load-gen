d42568c5b5aad22100d825ae8c297c5b47ef2e67
Martin <martin.rog0404@gmail.com>
2018-11-21 00:43:34 +0000

    fixing test

--- test/basic.test.js
+++ test/basic.test.js
@@ -5,6 +5,6 @@ describe("Header test", function(){
     it("Display header based on URL parameter", function(done){
         supertest(app).get("/start/1338")
         .expect(200)
-        .expect(/13388/, done)
+        .expect(/1338/, done)
     })
 });  
